bdc718d12fae435432c743675b7ba861b93a12e4
agnese-web <agnesefolkberga@gmail.com>
2021-06-18 20:40:57 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	// Integers
+	    int firstNumber = 5;
+	    int secondNumber = 6;
+	    int sum = firstNumber + secondNumber;
+	    //System.out.println(sum);
+
+		// Floats
+	    float firstFloat = 0.5f;
+	    float secondFloat = 2.3f;
+	    float result = firstFloat + secondFloat;
+	    //System.out.println(result);
+
+		// Doubles
+	    double secondDecimal = 0.7;
+	    double firstDecimal = 2.4;
+	    double decimalSum = secondDecimal + firstDecimal;
+//	    System.out.println(decimalSum);
+
+	    //Strings
+		String firstString = "chips";
+		String secondString = "chicken";
+		System.out.println("I love " + firstString + " and " + secondString);
+
+    }
+}
